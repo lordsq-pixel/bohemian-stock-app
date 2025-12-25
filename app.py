a0d086e3afc1e6ccada277eecfb17187c79bbc99
lordsq-pixel <lordsq@naver.com>
2025-12-25 18:51:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def analyze_stock(ticker, today):
 
 # --- 4. 메인 UI ---
 
-st.markdown('<H3 class="main-title">📊 나만의 AI AUTO STOCK.</H3>', unsafe_allow_html=True)
+st.markdown('<H3 class="main-title">📊 My Ai Auto Stock.</H3>', unsafe_allow_html=True)
 st.markdown('<p class="sub-title"># 프리미엄 빅데이터 분석기반 #</p>', unsafe_allow_html=True)
 
 market_type = st.sidebar.selectbox("대상 시장 선택", ["KOSPI", "KOSDAQ"])
@@ -140,3 +140,4 @@ st.markdown(f"""
         Copyright © 2025 보헤미안. All rights reserved.
     </div>
     """, unsafe_allow_html=True)
+
